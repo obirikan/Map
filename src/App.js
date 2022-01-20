57bb944cc5dabf232cf2c0026729e378aacc433f
unknown <obirikan020@gmail.com>
2022-01-20 10:10:02 -0800

    all

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {useState} from "react"
+import ReactMapGL,{Marker} from 'react-map-gl'
+import {Rooms} from ''
 function App() {
+  const [viewport, setViewport] =useState({
+    width:"100vw",
+    height:"100vh",
+    latitude: 37.7577,
+    longitude: -122.4376,
+    zoom: 8
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ReactMapGL
+      {...viewport}
+      mapboxApiAccessToken="pk.eyJ1Ijoia2Vsdmlua2FuIiwiYSI6ImNreW5hMmtkYTNvZnUyd3A4ZXQzdnA3YWkifQ.vO-0lRxsKwSrZJy8Tin_LQ"
+      onViewportChange={(viewport) => setViewport(viewport)}
+    >
+      <Marker
+      latitude= {37.7577}
+      longitude= {-122.4376}
+      offsetLeft={-20}
+      offsetTop={-10}
+      >
+        <h1>here</h1>
+      </Marker>
+    </ReactMapGL>
     </div>
   );
 }
